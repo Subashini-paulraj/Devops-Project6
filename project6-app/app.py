05422826e5f828f9ba9b48a35c4c32eac0952f2c
Subashini Paulraj <subashiniraj2000@gmail.com>
2026-02-12 22:34:50 +0530

    DB Name updated

--- project6-app/app.py
+++ project6-app/app.py
@@ -7,7 +7,7 @@ app = Flask(__name__)
 DB_HOST = os.getenv("DB_HOST")
 DB_USER = os.getenv("DB_USER")
 DB_PASS = os.getenv("DB_PASS")
-DB_NAME = "project6-mysql"
+DB_NAME = "project6db"
 
 
 def get_connection():
